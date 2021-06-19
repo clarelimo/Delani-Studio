03fc2fe63aff2e7caf983711495b575084dd0199
clarelimo <clare.limo@student.moringaschool.com>
2021-06-19 11:10:07 +0300

    created an array of objects to store the class names of the services

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+    var services =[{img:"design-img",info:"design-info"},{img:"dev-img",info:"dev-info"},{img:"product-img",info:"product-info"},]
+    
     $(".design-img").click(function(){
         $(".design-info").show();
         $(".design-img").hide();
