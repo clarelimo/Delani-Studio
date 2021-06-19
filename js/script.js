5aa6b16c70fa8ae3b27676f6ddc40724caaa735f
clarelimo <clare.limo@student.moringaschool.com>
2021-06-19 11:06:23 +0300

    show and hide services

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
-
+$(document).ready(function(){
+    $(".design-img").click(function(){
+        $(".design-info").show();
+        $(".design-img").hide();
+    });
+    $(".design-info").click(function(){
+        $(".design-img").show();
+        $(".design-info").hide();
+    })
+});
 
