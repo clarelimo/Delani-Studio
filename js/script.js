f7e4d4153333fac389093637928105f7fe66d816
clarelimo <clare.limo@student.moringaschool.com>
2021-06-20 08:10:05 +0300

    fuction to validate contact form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//services 
 $(document).ready(function(){
     var services =[{img:"design-img",info:"design-info"},{img:"dev-img",info:"dev-info"},{img:"product-img",info:"product-info"},]
     
@@ -13,6 +14,7 @@ $(document).ready(function(){
     })
 });
 
+//portfolio hover
 $(document).ready(function(){
     
     var projects =[{name:"work1",info:"work1Info"},{name:"work2",info:"work2Info"},{name:"work3",info:"work3Info"},{name:"work4",info:"work4Info"},{name:"work5",info:"work5Info"},{name:"work6",info:"work6Info"},{name:"work7",info:"work7Info"},{name:"work8",info:"work8Info"}];
@@ -25,3 +27,25 @@ $(document).ready(function(){
         });
     })
 });
+
+//form submission
+function submiForm(){
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var message = document.getElementById('message').value;
+    
+    if(!validateEmail(email)||email == "" || email == null){
+        alert("Please enter a valid email")
+    }else if(name == null || name == ""){
+        alert("Please enter your name")
+    }else if(message == null || message == ""){
+        alert("Please enter your message")
+    }else{
+        alert(name + " we have received your message. Thank you fro reaching out to us.")
+    }
+}
+
+function validateEmail($email) {
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailReg.test($email );
+  }
